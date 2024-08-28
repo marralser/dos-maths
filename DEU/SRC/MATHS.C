372ad033a78d9fd1223df4b6dbb6c1a81a28dd2a
marralser <142139652+marralser@users.noreply.github.com>
2024-08-28 08:28:28 +0200

    Add files via upload

---
+++ DEU/SRC/MATHS.C
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+void main(){
+	int choice;
+
+	clrscr();
+	while (1){
+	textcolor(YELLOW);
+	cprintf("Eelementary Math Training for Young Schoolkids \n\n");
+	printf("\n");
+	cprintf("What would you like to practice?\n\n");
+	printf("\n");
+	printf ("1. Additions / Plus - Rechnen\n");
+	printf ("2. Substractions / Minus - Rechnen\n");
+	printf ("3. Multiplication / Multiplizieren\n");
+	printf ("4. Division / Dividieren\n\n");
+	printf ("5. Enough for today, Genug geuebt?\n");
+	cprintf ("\nPlease choose / Bitte waehle? ");
+	scanf ("%d",&choice);
+
+	switch (choice) {
+	case 1:
+	system ("addjo.exe");
+	break;
+	case 2:
+	system ("subjo.exe");
+	break;
+	case 3:
+	system ("multijo.exe");
+	break;
+	case 4:
+	system ("divjo.exe");
+	break;
+	case 5:
+	printf ("Vielen Dank fuers ueben, Good Bye!\n");
+	exit (0);
+	default:
+	printf ("Try again / Versuchs nochmal :-)\n");
+	}
+ printf ("\n Press the ANY Key :-)");
+ getch();
+ clrscr();
+ }
+ }
+
---
+++ DEU/SRC/SUBJO2.C
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+
+#define MAX_JOKE_LENGTH 255
+#define MAX_JOKES 100
+
+int countJokes(char *filename) {
+    FILE *file;
+    int count = 0;
+    char line[MAX_JOKE_LENGTH];
+
+    if ((file = fopen(filename, "r")) == NULL) {
+        printf("Could not open jokes file.\n");
+        return 0;
+    }
+
+    while (fgets(line, MAX_JOKE_LENGTH, file) != NULL) {
+	if (line[0] != '\n') {
+            count++;
+        }
+    }
+
+    fclose(file);
+    return count;
+}
+
+void displayRandomJoke(char *filename, int totalJokes) {
+    FILE *file;
+    int jokeNum, currentLine = 0;
+    char line[MAX_JOKE_LENGTH];
+
+    if ((file = fopen(filename, "r")) == NULL) {
+        printf("Could not open jokes file.\n");
+        return;
+    }
+
+    jokeNum = rand() % totalJokes + 1;
+
+    while (fgets(line, MAX_JOKE_LENGTH, file) != NULL) {
+        currentLine++;
+        if (currentLine == jokeNum) {
+            printf("%s", line);
+            break;
+        }
+    }
+
+    fclose(file);
+}
+
+int main() {
+    int num1, num2, num3, num4, answer, totalJokes, score = 0, round  = 1, target = 10;
+    char again = 'n';
+    char filename[] = "jokes.txt";
+
+    srand((unsigned)time(NULL));
+
+    totalJokes = countJokes(filename);
+    if (totalJokes == 0) {
+        printf("No jokes found. Please check the jokes.txt file.\n");
+	return 1;
+    }
+
+    clrscr();
+    textcolor(YELLOW);
+    cprintf("Willkommen zum Minusrechnen - Quiz !\n");
+    printf("\nFÅr jede richtige Antwort sag ich dir einen Witz \noder einen FunFact.\n\n");
+    printf("\nWie viele Punkte moechtest du erreichen (Vorschlag: 10)? ");
+    scanf("%d", &target);
+    while (score < target ) {
+	num1 = (rand() % (70 - 3 + 1)) + 3 ;
+	num2 = (rand() % (30 - 3 + 1)) + 3;
+
+	if (num1 < num2) {
+	num4 = num1;
+	num1 = num2;
+	num2 = num4;
+	}
+	num3 = num1 - num2;
+	printf("\n\nRunde %d.\n\nWas ergibt %d minus %d? ", round, num1, num2);
+	round = round + 1;
+	scanf("%d", &answer);
+
+	if (answer == num3) {
+	    textcolor(YELLOW);
+	    score = score +1;
+	    printf("\n");
+	    cprintf("\nToll! Great job. Deine Punktezahl ist nun %d.", score);
+	    printf("\n");
+	    cprintf("Dafuer gibts nun einen FunFact oder Witz : \n\n");
+	    printf("\n");
+	    displayRandomJoke(filename, totalJokes);
+	} else {
+	    textcolor(RED);
+	    cprintf("\nOh nein. Das war nicht ganz richtig. Eigentlich ist die Antwort %d.\n", num3);
+	    textcolor(YELLOW);
+	}
+
+	printf("\n");
+	cprintf("\nDu hast jetzt %d Punkte gesammelt. Dir fehlen noch %d Punkte.", score, target - score);
+	fflush(stdin);
+
+    };
+    printf ("\n");
+    textcolor(GREEN);
+    cprintf("\nDanke fuers öben.");
+    printf("\n");
+    cprintf("Du hast insgesamt %d Runden gespielt, und davon %d Punkte gesammelt.\n Bis bald! \n", round -1, score);
+
+    return 0;
+}
+
